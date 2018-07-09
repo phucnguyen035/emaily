c6ad16e84d5727d643f990cb33e2259dc8254b2b
PhucNguyen <nguyen.hphuc035@gmail.com>
2018-07-10 00:30:54 +0700

    Partially complete authentication (need database to record user info)

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+// Routes
+const authRoutes = require('./routes/authRoutes');
+
+// Passport middleware
+require('./services/passport');
+
+// Initialize variables
 const app = express();
 const PORT = process.env.PORT || 5000;
 const db = process.env.DATABASE_URL || 'mongodb://localhost/emaily';
 
+// Database connection
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -18,6 +27,9 @@ app.get('/', (req, res) => {
   res.json({ message: 'hello world!' });
 });
 
-app.listen(PORT, (req, res) => {
+// Use routes
+app.use('/auth/', authRoutes);
+
+app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
