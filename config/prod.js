d570359bb62d30132d9d9a4018db518f85b99050
PhucNguyen <nguyen.hphuc035@gmail.com>
2018-07-12 11:55:38 +0700

    Initial setup for stripe

--- config/prod.js
+++ config/prod.js
@@ -2,4 +2,6 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   cookieKey: process.env.COOKIE_KEY,
+  stripePublic: process.env.STRIPE_PUBLIC,
+  stripeSecret: process.env.STRIPE_SECRET,
 };
